85341063ef5d1784cb87f8e732ca0a699a46aace
Oscar Legorreta <oscar_legorreta@epam.com>
2017-05-11 15:37:39 -0500

    Need this for consistency

--- SimpleAgileBoard.Web/Migrations/20170510145810_Seed.cs
+++ SimpleAgileBoard.Web/Migrations/20170510145810_Seed.cs
@@ -14,13 +14,13 @@ namespace SimpleAgileBoard.Web.Migrations
             using(var db = new AgileBoardDbContext()){
                 db.Tasks.AddRange(
                     new BoardTask[]{
-                        new BoardTask{ Id=Guid.NewGuid() , Name="Pending Stuff" , Status=SM.BoardTaskStatus.TO_DO },
+                        new BoardTask{ Id=new Guid("8753CA8C-6ECE-4410-AF11-517C3E0CB64F") , Name="Pending Stuff" , Status=SM.BoardTaskStatus.TO_DO },
                         new BoardTask{ Id=Guid.NewGuid() , Name="Start Actual Coding" , Status=SM.BoardTaskStatus.TO_DO },
                         new BoardTask{ Id=Guid.NewGuid() , Name="Get My Life Toghether" , Status=SM.BoardTaskStatus.TO_DO },
-                        new BoardTask{ Id=Guid.NewGuid() , Name="Procrastinate" , Status=SM.BoardTaskStatus.IN_PROGRESS },
+                        new BoardTask{ Id=new Guid("EC51D296-40E8-44E7-B56B-846DF44FD72E") , Name="Procrastinate" , Status=SM.BoardTaskStatus.IN_PROGRESS },
                         new BoardTask{ Id=Guid.NewGuid() , Name="Forget My Priorities" , Status=SM.BoardTaskStatus.IN_PROGRESS },
                         new BoardTask{ Id=Guid.NewGuid() , Name="Realize I'm Late" , Status=SM.BoardTaskStatus.IN_PROGRESS },
-                        new BoardTask{ Id=Guid.NewGuid() , Name="Give Up" , Status=SM.BoardTaskStatus.DONE },
+                        new BoardTask{ Id=new Guid("3988FF4C-ED57-4667-B351-6375EE2D8E11") , Name="Give Up" , Status=SM.BoardTaskStatus.DONE },
                         new BoardTask{ Id=Guid.NewGuid() , Name="Be Late" , Status=SM.BoardTaskStatus.DONE },
                     }
                 );
